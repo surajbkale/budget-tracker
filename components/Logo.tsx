2da85c4b7435440ca2756de203ff01653649235b
SurajKale01 <surajkalecsit@gmail.com>
2025-08-20 00:24:18 +0530

    Authentication signin and signup then added the navbar for both the mobile and desktop

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -15,4 +15,16 @@ function Logo() {
   );
 }
 
+export function LogoMobile() {
+  return (
+    <div>
+      <Link href={"/"} className="flex items-center gap-2">
+        <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-2xl font-bold leading-tight tracking-tight text-transparent">
+          BudgetTracker
+        </p>
+      </Link>
+    </div>
+  );
+}
+
 export default Logo;
