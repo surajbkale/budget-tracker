41513194fb7a830394538cd94d565fc4f3f15fa5
SurajKale01 <surajkalecsit@gmail.com>
2025-08-22 15:22:55 +0530

    feat: add CreateTransactionDialog component and transaction schema

---
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  Dialog,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { TransactionType } from "@/lib/types";
+import { cn } from "@/lib/utils";
+import {
+  CreateTransactionSchema,
+  CreateTransactionSchemaType,
+} from "@/schema/transaction";
+import { DialogContent } from "@radix-ui/react-dialog";
+import React, { ReactNode } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+interface Props {
+  trigger: ReactNode;
+  type: TransactionType;
+}
+
+function CreateTransactionDialog({ trigger, type }: Props) {
+  const form = useForm<CreateTransactionSchemaType>({
+    resolver: zodResolver(CreateTransactionSchema),
+    defaultValues: {
+      type,
+      date: new Date(),
+    },
+  });
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>{trigger}</DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Create a new{" "}
+            <span
+              className={cn(
+                "m-1",
+                type === "income" ? "text-emerald-500" : "text-red-500"
+              )}
+            >
+              {type}
+            </span>
+            transaction
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateTransactionDialog;
