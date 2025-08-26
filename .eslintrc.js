ce3531fd3df0d6169b78323d04859bd9bdaf1471
SurajKale01 <surajkalecsit@gmail.com>
2025-08-27 01:46:00 +0530

    added .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-require-imports": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
